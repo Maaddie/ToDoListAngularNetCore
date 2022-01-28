ab6aa29f912627ba5ffe2fc4d5ec54da3f8eb2a4
Madalina Varasteanu <madalinavarasteanu@rocketmortgage.com>
2022-01-27 22:44:23 -0500

    added the sql

---
+++ ToDoTasks.sql
@@ -0,0 +1,18 @@
+-- create database ToDo;
+
+-- create table ToDoTasks(
+-- id int primary key not null auto_increment,
+-- TmName nvarchar(30) not null,
+-- TaskName nvarchar(25),
+-- TaskDescription nvarchar(70),
+-- DueDate date,
+-- isCompleted bool
+-- );
+
+-- insert into ToDoTasks values(0, 'Rita Roe', 'Get groceries', 'Go to the store and get food', '2022-02-01', false ),
+-- (0, 'Bobby Brown', 'Wash floors', 'Get the mop and wash the floors', '2022-01-26', false),
+-- (0, 'Claire Martinez', 'Pick up meds', 'Go to the pharmacy and pick up the prescription', '2022-01-28', true),
+-- (0, 'Luke Dunphy', 'Pick up cake', 'Go to the bakery befoe closing and pick up the cake', '2022-01-30', false),
+-- (0, 'Kent Dunhill', 'Hang pictures', 'Put nails in the walls and hang pictures', '2022-02-05', true);
+
+select * from ToDoTasks;
